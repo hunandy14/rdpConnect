838eef6292c83cee1b02bd8640c87112bd74554e
Charlotte.Hong <hunandy14@gmail.com>
2022-03-18 22:55:28 +0900

    置中 - 改以裝置解析度為參數

--- rdpConnect.ps1
+++ rdpConnect.ps1
@@ -17,13 +17,16 @@ function rdpConnect {
     $title_h = 30
     $star_h  = 40
     
+    $device_w = $width - 16
+    $device_h = $height - ($title_h+$star_h+16)
+    
     $rdp = $rdp.Replace('${ip}'     ,$ip)
-    $rdp = $rdp.Replace('${width}'  ,$width - 16)
-    $rdp = $rdp.Replace('${height}' ,$height - ($title_h+$star_h+16))
+    $rdp = $rdp.Replace('${width}'  ,$device_w)
+    $rdp = $rdp.Replace('${height}' ,$device_h)
     $rdp = $rdp.Replace('${x1}'     ,$x1)
     $rdp = $rdp.Replace('${y1}'     ,$y1+7)
-    $rdp = $rdp.Replace('${x2}'     ,$x1+$width)
-    $rdp = $rdp.Replace('${y2}'     ,$y1+$height - $star_h)
+    $rdp = $rdp.Replace('${x2}'     ,$x1+16+$device_w)
+    $rdp = $rdp.Replace('${y2}'     ,$y1+16+$device_h+$title_h)
     
     $rdp > "Default.rdp"
     Set-Clipboard 'P@ssw0rd3'
